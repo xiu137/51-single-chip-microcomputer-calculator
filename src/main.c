8b6afc9e5c3e93f848ca0fb386b34b690a87e5ba
xiu137 <1919414254@qq.com>
2022-12-02 12:57:21 +0800

    double_calculate

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
     * 7  8  9 *
     * 4  5  6 /
     * 1  2  3 +
-    * ac 0  = -
+    * . 0  = -
     * 数字对应位置
     * 7  8  9 12
     * 4  5  6 13
@@ -18,23 +18,35 @@
 
 void main(){
     unsigned char KeyNumber;
-    unsigned int num1=0;
-    unsigned int num2=0;
+    double num1=0.0;
+    double num2=0.0;
     unsigned char i=1;
     unsigned char method=0;
-    unsigned int ans=0;
+    double ans=0;
+    unsigned char ansL=0;
+    unsigned char ansH=0;
+    unsigned char point=0;
     LCD_Init();
     while(1){
         KeyNumber=scanner();
         if(KeyNumber>=0&&KeyNumber<=9){
             LCD_ShowNum(0,0,KeyNumber,1);
             if (i==1)
-                num1=KeyNumber+num1*10;
+                if (point==0)
+                    num1=KeyNumber+num1*10;
+                else {
+                    num1=KeyNumber/10.0+num1;
+                }
             else
+                if (point==0)
                 num2=KeyNumber+num2*10;
+                else {
+                    num2=KeyNumber/10.0+num2;
+                }
         }
         else if(KeyNumber>=12&&KeyNumber<=15){
             i=2;
+            point=0;
             switch(KeyNumber){
                 case 12:
                     LCD_ShowChar(0,0,'/');method=1;break;
@@ -46,27 +58,35 @@ void main(){
                     LCD_ShowChar(0,0,'+');method=4;break;
             }
         }
-        else if(KeyNumber==10){//ac
-            LCD_Init();
-            num1=0;
-            num2=0;
-            method=0;
-            ans=0;
-            i=1;
+        else if(KeyNumber==10){//point
+            point=1;
+            LCD_ShowChar(0,0,'.');
         }
         else if(KeyNumber==11){//=
-            LCD_ShowChar(0,0,'=');
+            LCD_ShowChar(2,1,'=');
             switch(method){
                 case 2:
                     ans=num1*num2;break;
                 case 1:
                     ans=num1/num2;break;
                 case 3:
-                    ans=num1-num2;break;
+                    if(num1>=num2)
+                        ans=num1-num2;
+                    else{
+                        LCD_ShowChar(2,2,'-');
+                        ans=num2-num1;
+                        }
+                    break;
                 case 4:
                     ans=num1+num2;break;
             }
-            LCD_ShowNum(2,1,ans,4);
+            ansH=ans;
+            ansL=(ans-ansH)*100;
+            LCD_ShowNum(0,0,ansH,4);
+            if(ansL!=0){
+                LCD_ShowChar(0,4,'.');
+                LCD_ShowNum(0,5,ansL,2);
+            }
         }
     }
 }
